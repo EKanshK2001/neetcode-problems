fa421902e33e0f137b601377f65d8343b4bfd838
Ekansh Kandulna <efuforofficial@gmail.com>
2023-08-30 13:14:53 +0530

    some explanations edits

--- sliding-window/medium/longest-substring-without-repeating-characters/set-n/lswrc1.cpp
+++ sliding-window/medium/longest-substring-without-repeating-characters/set-n/lswrc1.cpp
@@ -6,25 +6,26 @@
  * link : https://leetcode.com/problems/longest-substring-without-repeating-characters/
  */
 
+/**
+* quite wierd of a sliding window.
+* we keep track of new elements encountered (or a valid substring) using a set 
+*
+* left pointer is actually a reference for the left corner of a substring and will also be used to calculate current length
+*
+* right pointer is the one traversing...
+* it keeps inserting new elements and if
+*
+* if it encounters old element from the whole of the substring (recall they are present in the set),
+* they will be popped using the reference of left pointer until that old element is fully new in the set/substring.
+*
+* thus the window (substring) slides and maxLength is recorded in the process.
+*
+* this method works fine but I really don't know how.
+*/
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        /*
-        quite wierd of a sliding window.
-        we keep track of new elements encountered using a set
-
-        left pointer is actually a reference for the length of substring
-
-        right pointer is the one traversing...
-        it keeps inserting new elements and if
-
-        if it encounters old element from the whole of the substring (recall they are present in the set),
-        they will be popped using the reference of left pointer until that old element is fully new in the set/substring.
-
-        thus the window (substring) slides and maxLength is recorded in the process.
-
-        this method works fine but I really don't know how.
-        */
         
         int maxLength = 0;
         int l = 0, r;
